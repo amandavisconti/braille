44c941fc832e3aaf56859b28e8e43baee3ea1377
Amanda Visconti <visconti@umd.edu>
2013-01-02 07:16:27 -0500

    Commented reprint plugin code

--- wp-plugin-braille/reprint/reprint.php
+++ wp-plugin-braille/reprint/reprint.php
@@ -3,48 +3,46 @@
 /*
 Plugin Name: Reprint
 Plugin URI: http://www.wordpress.org
-Description: Takes post text bracketed by shortcode and reprints on screen.
+Description: Takes post text bracketed by shortcode and reprints on screen with styling.
 Version: 1.0
 Author: Amanda Visconti (literaturegeek)
 Author URI: http://literaturegeek.com
-License: GNU
 */
 
-/*  Copyright 2012  Amanda Visconti (@literature_geek)
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 2, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+// This begins the admin section of the code (setting up the basic Wordpress plugin framework, adding a dashboard menu item, and creating an empty settings config page)
 
-// Admin stuff starts here (set up plugin, add menu item, empty config page)
-
-// Pulls any CSS you've added to the css/reprint.css file
+// Pulls any CSS you've added to the css/reprint.css file into the head of the displayed page so can be applied to the shortcoded text
 function avreprint_css() {
         echo '<link type="text/css" rel="stylesheet" href="' . plugins_url( 'css/reprint.css', __FILE__ ) . '" />' . "\n";
 }
 
-// Add a menu item for the plugin and test for authority
+// Adds a menu item to the WP dashboard for the plugin
 add_action( 'admin_menu', 'avreprint_menu' );
 
+// As part of the menu item, add an options (settings config) page
 function avreprint_menu() {
     add_options_page( 'avreprint Options', 'avreprint', 'manage_options', 'avreprint', 'avreprint_options' );
 }
 
+//  Check if you have the correct role to manage plugin options; reject if not.
 function avreprint_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
     }
 }
 
-// create custom plugin settings menu
+// Creates the custom plugin settings menu
 add_action('admin_menu', 'avreprint_create_menu');
 
 function avreprint_create_menu() {
 
-//create new top-level menu
+// Creates new top-level menu page
 	add_menu_page('avreprint Plugin Settings', 'Reprint Settings', 'administrator', __FILE__, 'avreprint_settings_page',plugins_url('/images/icon.png', __FILE__));
 }
 
 function avreprint_settings_page() {
-// Appearance of settings page
+// Sets appearance of settings page with a simple, non-functioning "submit" form
+// TO DO: edit form to allow settings config
 echo '<div class="wrap">
 	<h2>Reprint Plugin Options</h2>
 	<form method="post" action="options.php">
@@ -54,22 +52,22 @@ echo '<div class="wrap">
 </div>';
 }
 
-// End admin code, begin main plugin function code
-// Take text wrapped in [reprint] shortcode and reprint it somewhere on screen
+// The admin code section ends here. The main plugin function code begins.
+
+// Takes text wrapped in [reprint] shortcode and reprints it somewhere on screen (determined by styling in css/reprint.css)
 
 function getavreprint( $atts, $content = null ) {
-// Wrap in CSS class to style
+// Wraps in CSS class to style
 		$content = wpautop(trim($content));
         return '<div class="avreprint">'. do_shortcode($content) .'</div>';
 }
 
-// Allow [reprint][/reprint] shortcode
+// Creates [reprint][/reprint] shortcode
 add_shortcode('reprint', 'getavreprint');
 
-// Add the CSS file to the header when the page loads
+// Adds the CSS file to the header when the page loads
 add_action('wp_head', 'avreprint_css');
 
-// TinyMCE button, or only as extra? (see PullQuote plugin as example)
-// Allow uninstall
+// TO DO: allow uninstall
 
 ?>
